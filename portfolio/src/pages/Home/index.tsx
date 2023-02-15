f74fd63ab50e19e17b97bc7aa6a5bd4a08c2e6dc
devluanchristian <luan_christian-r@hotmail.com>
2023-02-14 21:36:51 -0300

    mexendo no css

--- portfolio/src/pages/Home/index.tsx
+++ portfolio/src/pages/Home/index.tsx
@@ -2,20 +2,10 @@ import "./styles.css";
 
 const Home = () => {
   return (
-    <div>
-      <div className="topsection">
+    <div className="container-home">
+      <div className="container-title">
         <h1>Luan Christian Rocha Do Carmo</h1>
         <p>Desenvolvedor FullStack Jr</p>
-      </div>
-      <div className="avatarAndIcons">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
-          alt=""
-        />
-      </div>
-      <div className="dowloandCV">
-        <p>Baixar CV</p>
-        <p>Vamos conversar</p>
       </div>
     </div>
   );
